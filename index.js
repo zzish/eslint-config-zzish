f3ae5fe1b5902217f6e24e096bab236831edd0b8
Blai Pratdesaba <hello@blaipratdesaba.com>
2016-10-04 15:45:55 +0100

    Update config

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ module.exports = {
         "no-use-before-define": 0,
         "import/no-extraneous-dependencies": 0,
         "import/no-named-as-default": 0,
+        "import/prefer-default-export": 0,
         "react/prefer-stateless-function": 0,
         "react/jsx-no-bind": 0,
         "react/jsx-indent": [1, 4],

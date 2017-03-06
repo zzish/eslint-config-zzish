bd89c2c1d0380a5056bb754375b21fc4cbe94d8a
Blai Pratdesaba <hello@blaipratdesaba.com>
2017-03-06 16:46:20 +0000

    Commit stuff

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 module.exports = {
     "parser": "babel-eslint",
-    "extends": "eslint-config-airbnb",
+    "extends": [
+        "eslint-config-airbnb"
+    ],
     "plugins": [
         "react",
         "flowtype"
